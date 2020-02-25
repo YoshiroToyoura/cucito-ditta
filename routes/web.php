cad2ec9f4837b26cd306f9e3a5cdb88d8576538e
”Yoshiro <toyoura226@yahoo.co.jp>
2020-02-25 11:38:12 +0000

    make

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('handmade.cucitoditta.top');
+    return view('handmade/cucitoditta/top');
 });
 
 Route::group(['prefix' => 'handmade'], function() {
